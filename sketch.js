468be21c67c92359a9810aba6f45b7921fee6528
KitNicholson <140785170+KitNicholson@users.noreply.github.com>
2024-03-19 20:22:09 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,19 +11,13 @@ function draw() {
   noErase();
   noStroke();
 
-  for (let i=0; i<300; i++) {
-    let x = random(windowWidth);
-    let y = random(windowHeight);
-
-    fill(0);
-    rect(x,y,1,1);
-  }
-
+  // draw lines converging on mouse position
   stroke(255);
   strokeWeight(0.2);
   line(mouseX, 0, mouseX, windowHeight);
   line(0, mouseY, windowWidth, mouseY);
 
+  // randomly remove pixels
   erase();
   noStroke();
 
